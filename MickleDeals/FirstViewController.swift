82ed8fe008e0aee01663399cae9ca810a0ab5ac1
Patrick Chan <chaniat@685b3588e5f5.local>
2015-08-02 17:02:11 -0700

    Initial Commit

---
+++ MickleDeals/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MickleDeals
+//
+//  Created by Chan, Nicky on 8/2/15.
+//  Copyright © 2015 Chan, Nicky. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
