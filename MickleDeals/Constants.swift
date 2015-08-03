a9212ec63acb6933662d4ab68dd48a9e193719b0
Patrick Chan <chaniat@685b3588e5f5.local>
2015-08-02 21:43:59 -0700

    Init add tab sub controllers

---
+++ MickleDeals/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MickleDeals
+//
+//  Created by Chan, Nicky on 8/2/15.
+//  Copyright © 2015 Chan, Nicky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    
+    static let primaryColor = UIColor(red:0.94, green:0.42, blue:0.0, alpha:1.0)
+}
